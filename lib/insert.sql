f7397d8708f1ea9797ede6b3d6e695eb67bdc9c7
abdiaziz77 <abdiaziznoor20@gmail.com>
2025-07-01 17:43:26 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 12, 'M', 'brown', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 8, 'M', 'black', 'aggressive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 5, 'F', 'gray', 'playful', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 10, 'M', 'dark brown', 'serious', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 7, 'F', 'white', 'gentle', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 9, 'M', 'green', 'grumpy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 4, 'F', 'golden', 'curious', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 6, 'M', 'brown', 'mysterious', 0);
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,39 @@
+# 1. Select all female bears and return their name and age
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+SELECT name, age FROM bears WHERE sex = 'F';
 """
 
+# 2. Select all bears' names and order them alphabetically
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+SELECT name FROM bears ORDER BY name ASC;
 """
 
+# 3. Select all bears' names and ages that are alive, order from youngest to oldest
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC;
 """
 
+# 4. Select the oldest bear and return their name and age
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;
 """
+
+# 5. Select the youngest bear and return their name and age
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+SELECT name, age FROM bears ORDER BY age ASC LIMIT 1;
+"""
+
+# 6. Select the bear that is alive is false (i.e., not alive)
+select_bear_that_is_alive_is_false = """
+SELECT * FROM bears WHERE alive = 0;
+"""
+
+# 7. Select the bear with the temperament ‘goofy’
+select_bear_with_goofy_temperament = """
+SELECT * FROM bears WHERE temperament = 'goofy';
+"""
+
+# 8. Select the bear with id = 8 (Tim’s killer)
+select_bear_that_killed_Tim = """
+SELECT * FROM bears WHERE id = 8;
 """
